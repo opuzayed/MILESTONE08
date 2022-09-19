bb5677a9ed47f84c145afa23096d68619073775e
opuzayed <zayedopu4933@gmail.com>
2022-09-20 01:18:04 +0600

    pass object destructuring as parameter

--- MODULE-44/js/countries.js
+++ MODULE-44/js/countries.js
@@ -4,18 +4,21 @@ const loadCountries = () =>{
     .then(data =>displayCountries(data));
 }
 const displayCountries = countries =>{
-console.log(countries);
+console.log(countries[0]);
 const countriesHTML = countries.map(country => getCountriesHTML(country));
 //console.log(countriesHTML);
 const container = document.getElementById('countries');
 container.innerHTML = countriesHTML.join(' ');
 
 }
-const getCountriesHTML = country =>{
+//----pass destructuring object parameter-----------
+const getCountriesHTML = ({name,flags,area}) =>{
+
     return `
     <div class='country'>
-    <h2>${country.name.common}</h2>
-    <img src = '${country.flags.png}'>
+    <h2>${name.common}</h2>
+    <p>Area : ${area}</p>
+    <img src = '${flags.png}'>
     </div>
     `
 }
